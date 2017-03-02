b8de24b1d59317c08529f88a54c078dbcc82930e
Eddie Sholl <eddie.sholl@gmail.com>
2017-03-02 20:01:26 +1100

    display result or error on page

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,16 +3,26 @@ import Ember from 'ember';
 var ix = 0;
 
 export default Ember.Controller.extend({
+  entity: 'user',
+  field: 'name',
+  term: 'Cross',
+
   actions: {
     submitQuery: function() {
       const entity = this.get('entity');
       const field = this.get('field');
       const term = this.get('term');
 
-      const result = this.get('store').queryRecord('search-result', {
+      this.get('store').queryRecord('search-result', {
         entity,
         field,
         term
+      })
+      .then(r => {
+        this.set('result', r);
+      })
+      .catch(e => {
+        this.set('result', 'An error occurred when getting results: ' + e);
       });
     }
   }
