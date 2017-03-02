e33fa86977e86e2846bcb03f9a3813c418782679
Eddie Sholl <eddie.sholl@gmail.com>
2017-03-02 20:23:33 +1100

    use raw ajax to submit request

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -7,22 +7,36 @@ export default Ember.Controller.extend({
   field: 'name',
   term: 'Cross',
 
+  status: 'No query has been submitted yet',
+
   actions: {
     submitQuery: function() {
       const entity = this.get('entity');
       const field = this.get('field');
       const term = this.get('term');
 
-      this.get('store').queryRecord('search-result', {
-        entity,
-        field,
-        term
-      })
-      .then(r => {
-        this.set('result', r);
-      })
-      .catch(e => {
-        this.set('result', 'An error occurred when getting results: ' + e);
+      const description = `entity:${entity} - field:${field} - term:${term}`;
+
+      this.set('status', 'Running query for ' + description);
+
+      const self = this;
+
+      $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8000',
+        data: {
+          entity,
+          field,
+          term
+        },
+
+        success: function(data) {
+          self.set('result', JSON.stringify(data, 0, 4));
+          self.set('status', `Displaying query result for ${description}`)
+        },
+        error: function(request, textStatus, errorThrown) {
+          self.set('status', errorThrown)
+        }
       });
     }
   }
